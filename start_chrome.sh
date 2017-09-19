015e7a5deedd0cf9cb128a6ea6ad8d8cd0753719
Thorsten Hans <thorsten.hans@gmail.com>
2017-09-19 08:22:39 +0200

    feat(devtime): add docker-compose and chrome start script for local env (non-https)

---
+++ start_chrome.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome http://10.211.55.2:8080/ --user-data-dir=/tmp/foo --unsafely-treat-insecure-origin-as-secure=http://10.211.55.2:8080/
